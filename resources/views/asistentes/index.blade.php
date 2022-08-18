08451eca0ee8257bfdf80ea8391926ed716583a8
Hiroshi Taco <hiroshitaco@gmail.com>
2022-08-18 02:44:13 -0500

    se agrego required

--- resources/views/asistentes/index.blade.php
+++ resources/views/asistentes/index.blade.php
@@ -41,33 +41,33 @@
                         <div class="row mb-3">
                             <div class="col-sm-6">
                                 <label for="inputEmail3" class="col-sm-12 col-form-label">Apellido Paterno</label>
-                                <input type="text" class="form-control" name="ap_paterno">
+                                <input type="text" class="form-control" name="ap_paterno" required>
                             </div>
                             <div class="col-sm-6">
                                 <label for="inputEmail3" class="col-sm-12 col-form-label">Apellido Materno</label>
-                                <input type="text" class="form-control" name="ap_materno">
+                                <input type="text" class="form-control" name="ap_materno" required>
                             </div>
                         </div>
                         <div class="row mb-3">
                             <div class="col-sm-12">
                                 <label for="inputEmail3" class="col-sm-6 col-form-label">Nombre Completo</label>
-                                <input type="text" class="form-control" name="nombre">
+                                <input type="text" class="form-control" name="nombre" required>
                             </div>
                         </div>
                         <div class="row mb-3">
                             <div class="col-sm-4">
                                 <label for="inputEmail3" class="col-sm-12 col-form-label">Nro. de DNI</label>
-                                <input type="text" class="form-control" name="nro_dni">
+                                <input type="text" class="form-control" name="nro_dni"required>
                             </div>
                             <div class="col-sm-4">
                                 <label for="inputEmail3" class="col-sm-12 col-form-label">Fecha de Nacimiento</label>
                                 <input type="date" id="start" name="fecha_nac"
                                        value="2018-07-22"
-                                       min="2018-01-01" max="2018-12-31">
+                                       min="2018-01-01" max="2018-12-31"required>
                             </div>
                             <div class="col-sm-4">
                                 <label for="inputEmail3" class="col-sm-12 col-form-label">Sexo</label>
-                                <select class="form-select mb-3" aria-label="Default select example" name="sexo">
+                                <select class="form-select mb-3" aria-label="Default select example" name="sexo" required>
                                     <option selected>Seleccione</option>
                                     <option value="1">Masculino</option>
                                     <option value="2">Femenino</option>
@@ -78,27 +78,27 @@
                         <div class="row mb-3">
                             <div class="col-sm-6">
                                 <label for="inputEmail3" class="col-sm-12 col-form-label">Lugar de Nacimiento</label>
-                                <input type="text" class="form-control" name="lug_nacimiento">
+                                <input type="text" class="form-control" name="lug_nacimiento" required>
                             </div>
                             <div class="col-sm-6">
                                 <label for="inputEmail3" class="col-sm-12 col-form-label">Ubicacion Actual</label>
-                                <input type="text" class="form-control" name="ubicacion_actual">
+                                <input type="text" class="form-control" name="ubicacion_actual" required>
                             </div>
                         </div>
                         <div class="row mb-3">
                             <div class="col-sm-7">
                                 <label for="inputEmail3" class="col-sm-12 col-form-label">Centro Laboral Actual</label>
-                                <input type="text" class="form-control" name="cent_lab_actual">
+                                <input type="text" class="form-control" name="cent_lab_actual" required>
                             </div>
                             <div class="col-sm-5">
                                 <label for="inputEmail3" class="col-sm-12 col-form-label">Cargo Actual</label>
-                                <input type="text" class="form-control" name="cargo_actual">
+                                <input type="text" class="form-control" name="cargo_actual" required>
                             </div>
                         </div>
                         <div class="row mb-3">
                             <div class="col-sm-4">
                                 <label for="inputEmail3" class="col-sm-12 col-form-label">Grado Academico</label>
-                                <select class="form-select mb-3" aria-label="Default select example" name="grado_academico">
+                                <select class="form-select mb-3" aria-label="Default select example" name="grado_academico" required>
                                     <option selected>-</option>
                                     <option value="1">Doctor</option>
                                     <option value="2">Maestro</option>
@@ -109,17 +109,17 @@
                             </div>
                             <div class="col-sm-8">
                                 <label for="inputEmail3" class="col-sm-12 col-form-label">Profesion</label>
-                                <input type="text" class="form-control" name="profesion">
+                                <input type="text" class="form-control" name="profesion" required>
                             </div>
                         </div>
                         <div class="row mb-3">
                             <div class="col-sm-6">
                                 <label for="inputEmail3" class="col-sm-12 col-form-label">Correo Electronico</label>
-                                <input type="email" class="form-control" name="email">
+                                <input type="email" class="form-control" name="email" required>
                             </div>
                             <div class="col-sm-6">
                                 <label for="inputEmail3" class="col-sm-12 col-form-label">Numero de Celular</label>
-                                <input type="number" class="form-control" name="num_celular">
+                                <input type="number" class="form-control" name="num_celular" required>
                             </div>
                         </div>
                         <fieldset class="row mb-3">
@@ -128,11 +128,11 @@
                                 <div class="container">
 
                                     <label class="radio-inline form-check-label">
-                                        <input class="form-check-input" type="radio" name="agremiado" value="1" checked>Si
+                                        <input class="form-check-input" type="radio" name="agremiado" value="1" checked required>Si
                                     </label>
 
                                     <label class="radio-inline form-check-label">
-                                        <input class="form-check-input" type="radio" name="agremiado" value="2" >No
+                                        <input class="form-check-input" type="radio" name="agremiado" value="2" required >No
                                     </label>
 
 
@@ -144,7 +144,7 @@
                         <div class="row mb-3">
                             <div class="col-sm-6">
                                 <label for="inputEmail3" class="col-sm-12 col-form-label">Numero de Colegiatura (Solo si corresponde)</label>
-                                <input type="number" class="form-control" name="nro_colegiatura">
+                                <input type="number" class="form-control" name="nro_colegiatura" required>
                             </div>
                         </div>
                         <div class="row mt-6">
@@ -160,8 +160,21 @@
     <!-- Form End -->
     <!-- Content End -->
     <!-- Back to Top -->
-    <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
 </div>
+
+<div class="container-fluid">
+        <div class="bg-secondary rounded-top p-4">
+            <div class="row">
+                <div class="col-12 col-sm-6 text-center text-sm-start">
+                    <a href="#"> Capitulo de Ingenieros Industriales y de Sistemas </a> 
+                </div>
+                <div class="col-12 col-sm-6 text-center text-sm-end">
+                    <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
+                    CIP PUNO 
+                </div>
+            </div>
+        </div>
+    </div>
 
 <!-- JavaScript Libraries -->
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
