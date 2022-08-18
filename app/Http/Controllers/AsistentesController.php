7338168ea1f8330d2eef24e468d4d4707977625a
clintonkenedy <clintonventuracondori@gmail.com>
2022-08-18 02:29:15 -0500

    creacion de controlador

--- app/Http/Controllers/AsistentesController.php
+++ app/Http/Controllers/AsistentesController.php
@@ -15,7 +15,7 @@ class AsistentesController extends Controller
     public function index()
     {
         //
-
+        return view('asistentes.index');
     }
 
     /**
@@ -37,29 +37,30 @@ class AsistentesController extends Controller
     public function store(Request $request)
     {
         //validate
-        $request->validate([
-
-        ]);
+        //dd($request);
         //
         $asistente = new Asistentes;
-        $asistente->apellido_p = ;
-        $asistente->apellido_m = ;
-        $asistente->nombre = ;
-        $asistente->dni = ;
-        $asistente->fecha_nacimiento = ;
-        $asistente->sexo = ;
-        $asistente->ubi_actual = ;
-        $asistente->centro_lab_act = ;
-        $asistente->cargo = ;
-        $asistente->grado_academico = ;
-        $asistente->profesion = ;
-        $asistente->correo = ;
-        $asistente->num_celular = ;
-        $asistente->agremiado = ;
-        $asistente->num_colegiatura = ;
+        $asistente->apellido_p = $request->ap_paterno;
+        $asistente->apellido_m = $request->ap_materno;
+        $asistente->nombre = $request->nombre;
+        $asistente->dni = $request->nro_dni;
+        $asistente->fecha_nacimiento = $request->fecha_nac;
+        $asistente->sexo = $request->sexo;
+        $asistente->lugar_nac = $request->lug_nacimiento;
+        $asistente->ubi_actual = $request->ubicacion_actual;
+        $asistente->centro_lab_act = $request->cent_lab_actual;
+        $asistente->cargo = $request->cargo_actual;
+        $asistente->grado_academico = $request->grado_academico;
+        $asistente->profesion = $request->profesion;
+        $asistente->correo = $request->email;
+        $asistente->num_celular = $request->num_celular;
+        $asistente->agremiado = $request->agremiado;
+        $asistente->num_colegiatura = $request->nro_colegiatura;
+        //dd($asistente);
         $asistente->save();
 
         //respuesta
+        return redirect()->route('asistentes.index');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\Asistentes;
+use App\Http\Controllers\AsistentesController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,4 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('asistentes',Asistentes::class);
+Route::resource('asistentes',AsistentesController::class);
